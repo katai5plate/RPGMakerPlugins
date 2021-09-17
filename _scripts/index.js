d708e184b6bedbacaea26219bbea24536411fdc2
katai5plate <katai5plate.sub@gmail.com>
2021-09-17 17:50:07 +0900

    remove br

--- _scripts/index.js
+++ _scripts/index.js
@@ -62,11 +62,9 @@ const help = (man) => console.log(`USAGE:\n  ${man}\n`);
             .map(({ path, name }) =>
               [
                 `\n### ${name}`,
-                `\n\`\`\`\n${fs
-                  .readFileSync(resolve(path, "./src/help.txt"), {
-                    encoding: "utf8",
-                  })
-                  .replace(/\n/g, "<br/>\n")}`,
+                `\n\`\`\`\n${fs.readFileSync(resolve(path, "./src/help.txt"), {
+                  encoding: "utf8",
+                })}`,
                 `\n\`\`\`\n- [ダウンロードはこちら(Rawボタンを右クリックして保存)](https://github.com/katai5plate/RPGMakerPlugins/blob/main/plugins/${target}/${name}/dist/${name}.js)`,
               ].join("\n")
             )
