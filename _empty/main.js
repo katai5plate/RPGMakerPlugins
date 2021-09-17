3cfcf15e56f81a62ad4bc4206129ce11f88f2a48
katai5plate <katai5plate.sub@gmail.com>
2021-09-17 15:38:26 +0900

    add create/watch scripts

---
+++ _empty/main.js
@@ -0,0 +1,1 @@
+/// <reference path="../../../_templates/utils.js"/>
--- _scripts/index.js
+++ _scripts/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs-extra");
+const chokidar = require("chokidar");
 const build = require("./build");
-const f = require("./build/ano");
+const { resolve } = require("./utils");
 
 const [, , name, ...args] = process.argv;
 
@@ -8,26 +9,43 @@ const help = (man) => console.log(`USAGE:\n  ${man}\n`);
 
 //
 (() => {
+  if (name === "create") {
+    if (!args[0] || !args[1]) {
+      return help("build [mv|mz] [pluginName]");
+    }
+    if (!["mv", "mz"].includes(args[0])) throw new Error("無効なターゲット");
+    const path = `./plugins/${args[0]}/${args[1]}`;
+    fs.copySync("./_empty", resolve(path, "./src"));
+    fs.mkdirp(resolve(path, "./dist"));
+  }
   if (name === "build") {
     if (!args[0]) {
       return help("build [mv|mz] [pluginName]");
     }
+    if (!["mv", "mz"].includes(args[0])) throw new Error("無効なターゲット");
     build(args[0], args[1]);
   }
   if (name === "build-all") {
     console.log("mv");
-    fs.readdirSync("./plugins/mv/")
-      .filter((n) => n.match(/^H2A_/))
-      .forEach((n) => {
-        console.log(n);
-        build("mv", n);
-      });
+    fs.readdirSync("./plugins/mv/").forEach((n) => {
+      console.log(n);
+      build("mv", n);
+    });
     console.log("mz");
-    fs.readdirSync("./plugins/mz/")
-      .filter((n) => n.match(/^H2A_/))
-      .forEach((n) => {
-        console.log(n);
-        build("mz", n);
-      });
+    fs.readdirSync("./plugins/mz/").forEach((n) => {
+      console.log(n);
+      build("mz", n);
+    });
+  }
+  if (name === "watch") {
+    if (!args[0]) {
+      return help("watch [mv|mz] [pluginName]");
+    }
+    if (!["mv", "mz"].includes(args[0])) throw new Error("無効なターゲット");
+    const path = resolve(`./plugins/${args[0]}/${args[1]}/`);
+    console.log("WATCHING...", path);
+    chokidar.watch(path, { ignored: /\/dist\/.*?\.js/ }).on("change", () => {
+      build(args[0], args[1]);
+    });
   }
 })();
