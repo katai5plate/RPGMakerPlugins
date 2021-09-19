cade9a97397aebd5cee3124dc5c488957a6667d3
katai5plate <katai5plate.sub@gmail.com>
2021-09-19 15:05:14 +0900

    add cors

--- serve.js
+++ serve.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const cors = require("cors");
+//
 ((app) => {
+  app.use(cors());
   app.use(express.static("."));
   app.listen(3333, () => {
     console.log("Open http://localhost:3333/");
