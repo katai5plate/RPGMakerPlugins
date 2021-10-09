3d247f1d5b83cc026401656e912476c32c85183e
katai5plate <katai5plate.sub@gmail.com>
2021-10-10 06:50:50 +0900

    fix path

--- js/plugins/mz/H2A_UIPicture/src/base.js
+++ js/plugins/mz/H2A_UIPicture/src/base.js
@@ -2,6 +2,6 @@
   /***__INCLUDE="../../../_templates/utils.js"__***/
   /***__INCLUDE="../../../_templates/debug.js"__***/
   /***__INCLUDE="./extension.js"__***/
-  /***__INCLUDE="./math.js"__***/
+  /***__INCLUDE="./calc.js"__***/
   /***__INCLUDE="./main.js"__***/
 })();
